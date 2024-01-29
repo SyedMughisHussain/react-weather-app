8c1a762f7d597c4f2d37be643e1478187e531ed4
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-01-29 22:32:35 +0500

    Chnages made

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,16 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./Form";
 import axios from "axios";
 
 const Welcome = () => {
 
-  const getData = (data) => {
-    console.log(data);
-  } 
+  let [weatherData, setWeatherData] = useState({}); 
+
+  const getData = (cityName) => {
+    console.log(cityName);
+    // setWeatherData([]);
+    fetchWeatherData(cityName);
+    console.log(weatherData);
+    // setWeatherData([...weatherData]);
+  };
+
+  useEffect(() => {
+    fetchWeatherData("Karachi");
+  }, []);
+
+  const fetchWeatherData = async (cityName) => {
+    // setWeatherData([])
+    const apikey = `ed9762be5575457144a931c00af77267`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`;
+    const response = await axios.get(url);
+    setWeatherData(response.data);
+  };
+
+  console.log(weatherData);
 
   return (
     <div className="h-[450px] shadow-2xl shadow-slate-700 w-[700px] rounded-xl mt-[40px]">
       <Form getData={getData} />
+      {
+        weatherData.length === 0 ? <h1>Loading...</h1> : <h1>Data Fetched</h1> 
+      }
+
     </div>
   );
 };
