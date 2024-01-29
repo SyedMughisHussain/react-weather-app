0463286e4e2a76e2f0a87bda6f4ad26e94d0e508
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-01-29 16:15:16 +0500

    Changes made

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,16 @@ import Welcome from "./Welcome";
 
 const App = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Welcome />
-    </div>
+    <>
+      <div className="h-[500px] flex justify-center items-center">
+        <Welcome />
+      </div>
+      <div className="text-center">
+        <span className="text-blue-300 font-bold" >Coded by </span>
+        <span className="text-[#AC5183] font-bold"><a href="https://github.com/SyedMughisHussain">Syed Mughis Hussain, </a></span>
+        <span className="text-blue-300 font-bold">Hosted on Vercel.</span>
+      </div>
+    </>
   );
 };
 
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ const Welcome = () => {
   return (
     <div className='bg-white h-[450px] shadow-2xl w-[450px] rounded-md'>
         <p className='text-center text-[20px] font-bold mt-7'>React Weather App</p>
-        <img src="../assets/icons/perfect-day.svg" alt="Perfect Day"/>
+        <img src="/assets/icons/perfect-day.svg" alt="Perfect Day Image" />
     </div>
   )
 }
