df90133607507c6ea303d95edbf8c9052a3c96d3
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-01-30 11:45:50 +0500

    Changes made

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Form from "./Form";
 import axios from "axios";
+import Container from "./Container";
 
 const Welcome = () => {
 
-  let [weatherData, setWeatherData] = useState({}); 
+  const [weatherData, setWeatherData] = useState(null); 
 
   const getData = (cityName) => {
     console.log(cityName);
-    // setWeatherData([]);
     fetchWeatherData(cityName);
     console.log(weatherData);
-    // setWeatherData([...weatherData]);
   };
 
   useEffect(() => {
-    fetchWeatherData("Karachi");
+    const getData = async () => {
+    const weather = await fetchWeatherData("Karachi");
+    setWeatherData(weather);
+    }
+    getData();
   }, []);
 
   const fetchWeatherData = async (cityName) => {
-    // setWeatherData([])
     const apikey = `ed9762be5575457144a931c00af77267`;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`;
     const response = await axios.get(url);
     setWeatherData(response.data);
+    return response.data;
   };
 
   console.log(weatherData);
@@ -31,10 +34,7 @@ const Welcome = () => {
   return (
     <div className="h-[450px] shadow-2xl shadow-slate-700 w-[700px] rounded-xl mt-[40px]">
       <Form getData={getData} />
-      {
-        weatherData.length === 0 ? <h1>Loading...</h1> : <h1>Data Fetched</h1> 
-      }
-
+      { weatherData === null ? <h1>Loading..</h1> : <Container />} 
     </div>
   );
 };
